8cca911280bd7a52ed455c8ae368500cdb12a495
ststeiger <quandary82@hailmail.net>
2015-08-26 09:04:57 +0200

    Configurable data-source

--- SqlServerDataFeedEF6/ExcelDataFeed.svc.cs
+++ SqlServerDataFeedEF6/ExcelDataFeed.svc.cs
@@ -36,6 +36,48 @@ namespace SqlServerDataFeedEF6
     public class ExcelDataFeed : System.Data.Services.Providers.EntityFrameworkDataService<COR_Basic_DemoEntities>
     {
 
+        //// Unnecessary
+        //public static void ModifyEntityConnectionString()
+        //{
+        //    System.Data.SqlClient.SqlConnectionStringBuilder csb = new System.Data.SqlClient.SqlConnectionStringBuilder();
+        //    System.Data.EntityClient.EntityConnectionStringBuilder ecb = new System.Data.EntityClient.EntityConnectionStringBuilder();
+
+
+        //    ecb.Metadata = "res://*/Sample.csdl|res://*/Sample.ssdl|res://*/Sample.msl";
+        //    ecb.Provider = "System.Data.SqlClient";
+        //    ecb.ProviderConnectionString = csb.ConnectionString;
+
+        //    // COR_Basic_DemoEntities dataContext = new COR_Basic_DemoEntities(ecb.ConnectionString);
+        //}
+
+
+        public static string GetConnectionString()
+        {
+            System.Data.SqlClient.SqlConnectionStringBuilder csb = new System.Data.SqlClient.SqlConnectionStringBuilder();
+            csb.DataSource = Environment.MachineName;
+            csb.InitialCatalog = "COR_Basic_Demo";
+            csb.IntegratedSecurity = true;
+            csb.ApplicationName = "SqlServerDataFeed";
+
+            if (!csb.IntegratedSecurity)
+            {
+                csb.UserID = "";
+                csb.Password = "";
+            }
+
+            return csb.ConnectionString;
+        }
+
+
+        protected override COR_Basic_DemoEntities CreateDataSource()
+        {
+            //COR_Basic_DemoEntities ctx = new COR_Basic_DemoEntities(connectionString);
+            COR_Basic_DemoEntities ctx = new COR_Basic_DemoEntities();
+            ctx.Database.Connection.ConnectionString = GetConnectionString();
+
+            return ctx;
+        }
+
 
         // This method is called only once to initialize service-wide policies.
         public static void InitializeService(DataServiceConfiguration config)
---
+++ SqlServerDataFeedEF6/SQL/Augen.sql
@@ -0,0 +1,4 @@
+﻿
+ALTER TABLE dbo.T_Benutzer ADD BE_Augen int NULL
+
+ALTER TABLE dbo.T_Benutzer DROP COLUMN BE_Augen 
